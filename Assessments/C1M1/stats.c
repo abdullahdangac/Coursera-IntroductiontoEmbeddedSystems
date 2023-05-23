f84f9408a0ae5334857948cdab3aa722004ebae0
Abdullah Dangaç <103657751+abdullahdangac@users.noreply.github.com>
2023-05-23 20:56:38 +0300

    Update stats.c

--- Assessments/C1M1/stats.c
+++ Assessments/C1M1/stats.c
@@ -24,9 +24,24 @@
  * @date 2023-4-30
  *
  */
-
 #include <stdio.h>
 #include "stats.h"
+
+/* Size of the data Set */
+#define SIZE (40)
+
+void main() {
+
+  // test data set
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+  
+  print_statistics(test, SIZE);
+  return 0;
+}
 
 void print_array(const unsigned char* arr, unsigned int size) {
   int i;
